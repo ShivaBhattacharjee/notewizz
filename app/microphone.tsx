10658fa7e18117b59df775739d17cdff65f9c6d1
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2024-04-15 13:45:42 +0530

    captions work now

--- app/microphone.tsx
+++ app/microphone.tsx
@@ -1,19 +1,23 @@
 "use client";
 
 import {
+  CreateProjectKeyResponse,
   LiveClient,
   LiveTranscriptionEvents,
   createClient,
 } from "@deepgram/sdk";
 import { useState, useEffect, useCallback } from "react";
 import { useQueue } from "@uidotdev/usehooks";
+// import Dg from "./dg.svg";
 import Recording from "./recording.svg";
 import Image from "next/image";
 
 export default function Microphone() {
   const { add, remove, first, size, queue } = useQueue<any>([]);
+  const [apiKey, setApiKey] = useState<CreateProjectKeyResponse | null>();
   const [connection, setConnection] = useState<LiveClient | null>();
   const [isListening, setListening] = useState(false);
+  const [isLoadingKey, setLoadingKey] = useState(true);
   const [isLoading, setLoading] = useState(true);
   const [isProcessing, setProcessing] = useState(false);
   const [micOpen, setMicOpen] = useState(false);
@@ -52,11 +56,27 @@ export default function Microphone() {
     }
   }, [add, microphone, userMedia]);
 
+  useEffect(() => {
+    if (!apiKey) {
+      console.log("getting a new api key");
+      fetch("/api", { cache: "no-store" })
+        .then((res) => res.json())
+        .then((object) => {
+          if (!("key" in object)) throw new Error("No api key returned");
+
+          setApiKey(object);
+          setLoadingKey(false);
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    }
+  }, [apiKey]);
 
   useEffect(() => {
-
+    if (apiKey && "key" in apiKey) {
       console.log("connecting to deepgram");
-      const deepgram = createClient(process.env.NEXT_PUBLIC_DEEPGRAM_API_KEY ?? "");
+      const deepgram = createClient(apiKey?.key ?? "");
       const connection = deepgram.listen.live({
         model: "nova",
         interim_results: true,
@@ -71,22 +91,25 @@ export default function Microphone() {
       connection.on(LiveTranscriptionEvents.Close, () => {
         console.log("connection closed");
         setListening(false);
+        setApiKey(null);
         setConnection(null);
       });
 
       connection.on(LiveTranscriptionEvents.Transcript, (data) => {
+        if (!data.is_final) return;
         const words = data.channel.alternatives[0].words;
         const caption = words
           .map((word: any) => word.punctuated_word ?? word.word)
-          .join(" ");
-        if (caption !== "") {
-          setCaption(caption);
+          .join(' ');
+        if (caption!== '') {
+          setCaption((cap) => cap + ' ' + caption);
         }
       });
 
       setConnection(connection);
       setLoading(false);
-    },[]);
+    }
+  }, [apiKey]);
 
   useEffect(() => {
     const processQueue = async () => {
@@ -109,6 +132,10 @@ export default function Microphone() {
     processQueue();
   }, [connection, queue, remove, first, size, isProcessing, isListening]);
 
+  if (isLoadingKey)
+    return (
+      <span className="w-full text-center">Loading ...</span>
+    );
   if (isLoading)
     return <span className="w-full text-center">Loading the app...</span>;
 
@@ -145,8 +172,7 @@ export default function Microphone() {
           />
         </button>
         <div className="mt-20 p-6 text-xl text-center">
-          {caption && micOpen
-            && caption}
+          {caption  && caption}
         </div>
       </div>
       <div
@@ -154,10 +180,15 @@ export default function Microphone() {
                   fixed bottom-0 right-5 rounded-lg mr-1 mb-5 lg:mr-5 lg:mb-5 xl:mr-10 xl:mb-10 gap-5"
       >
         <span className="text-sm text-gray-400">
-          {isListening
-            ? "socket connection open!"
-            : "socket is connecting..."}
+          {isListening ? " connection open!" : " connecting..."}
         </span>
+        {/* <Dg
+          width="30"
+          height="30"
+          className={
+            isListening ? "fill-white drop-shadow-glowBlue" : "fill-gray-600"
+          }
+        /> */}
       </div>
     </div>
   );
